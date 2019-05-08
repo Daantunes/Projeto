508ab0030af1cc9c530f637181815c0316db25d1
Daantunes <50405775+Daantunes@users.noreply.github.com>
2019-05-08 17:39:52 +0100

    Update do_trimmomatic_PE.sh

--- rna_seq/do_trimmomatic_PE.sh
+++ rna_seq/do_trimmomatic_PE.sh
@@ -7,7 +7,7 @@ docker pull quay.io/biocontainers/trimmomatic:0.39--1
 
 chmod 777 $1
 
-docker run -d -it --rm -v /usr/share/trimmomatic:/adapters/ \
+docker run -it --rm --user $(id -u):$(id -g) -v /usr/share/trimmomatic:/adapters/ \
 -v $1:/data/ quay.io/biocontainers/trimmomatic:0.39--1 \
 trimmomatic PE /data/$2 /data/$3 /data/TP_$2 /data/TU_$2 /data/TP_$3 /data/TU_$3 \
 ILLUMINACLIP:/adapters/TruSeq3-PE.fa:2:30:10 \
