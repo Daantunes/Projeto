1d234beb4d469af0b8c3d5436e7acb71e56f2012
Daantunes <50405775+Daantunes@users.noreply.github.com>
2019-05-08 17:37:00 +0100

    Update do_fastqc.sh

--- rna_seq/do_fastqc.sh
+++ rna_seq/do_fastqc.sh
@@ -9,4 +9,5 @@ docker pull biocontainers/fastqc:v0.11.5_cv3
 
 chmod 777 $1
 
-docker run -d --rm -u "$(id -u):$(id -g)" -t -v $1:/data/ biocontainers/fastqc:v0.11.5_cv3 fastqc $2
+docker run -d --rm -u "$(id -u):$(id -g)" -t -v $1:/data/ biocontainers/fastqc:v0.11.5_cv3 fastqc $2 \
+ > $1/results_$2/run.log
