1254b0a77a64c5b3d928495ea62123a62a9a9323
Daantunes <50405775+Daantunes@users.noreply.github.com>
2019-05-11 14:19:37 +0100

    Update do_qualimap.sh

--- rna_seq/do_qualimap.sh
+++ rna_seq/do_qualimap.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 if [ "$1" == "-h" ]; then
-  echo "<Pathway to fastq files> <Name of the fastq file>"
+  echo "<Pathway to BAM file> <Name of the BAM file> <Name of the GTF file>"
   exit 0
 fi
 
 docker pull quay.io/biocontainers/qualimap:2.2.2b--1
 
 docker run -it --rm --user $(id -u):$(id -g) -v $1:/data/ quay.io/biocontainers/qualimap:2.2.2b--1 \
-trimmomatic SE /data/$2 /data/T_$2 \
-ILLUMINACLIP:/adapters/TruSeq3-SE.fa:2:30:10 \
-MINLEN:36 #> $1/results_$2/run.log
+qualimap rnaseq -bam $1/$2 -gtf $1/$3 \
+-outformat PDF:HTML -outfile Qualimap_$2 \
+-outdir $1/results_qualimap #> $1/results_$2/run.log
