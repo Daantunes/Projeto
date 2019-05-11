889bc96a5a37852969ab09b74ce823f216e284c6
Daantunes <50405775+Daantunes@users.noreply.github.com>
2019-05-11 18:28:22 +0100

    Update do_trimmomatic_SE.sh

--- rna_seq/do_trimmomatic_SE.sh
+++ rna_seq/do_trimmomatic_SE.sh
@@ -11,12 +11,12 @@ cp /adapters/TruSeq2-SE.fa $1/TruSeqALL-SE.fa
 printf "\n" >> $1/TruSeqALL-SE.fa
 cat /adapters/TruSeq3-SE.fa >> $1/TruSeqALL-SE.fa
 
-docker run -it --rm --user $(id -u):$(id -g) -v /usr/share/trimmomatic:/adapters/ \
+docker run --rm --user $(id -u):$(id -g) -v /usr/share/trimmomatic:/adapters/ \
 -v $1:/data/ quay.io/biocontainers/trimmomatic:0.39--1 \
 trimmomatic SE /data/$2 /data/T_$2 \
 ILLUMINACLIP:$1/TruSeqALL-SE.fa:2:0:10 \
 LEADING:0 \
 TRAILING:0 \
 SLIDINGWINDOW:0:0 \
-MINLEN:36 > $1/results_$2/run.log
+MINLEN:36 > run$2.log 2>&1 &
 
