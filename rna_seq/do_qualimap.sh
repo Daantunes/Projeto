139ab803f15873f19d232ec306a5aaf5268174cc
Daantunes <50405775+Daantunes@users.noreply.github.com>
2019-05-11 18:26:30 +0100

    Update do_qualimap.sh

--- rna_seq/do_qualimap.sh
+++ rna_seq/do_qualimap.sh
@@ -6,7 +6,7 @@ fi
 
 docker pull quay.io/biocontainers/qualimap:2.2.2b--1
 
-docker run -it --rm --user $(id -u):$(id -g) -v $1:/data/ quay.io/biocontainers/qualimap:2.2.2b--1 \
+docker run --rm --user $(id -u):$(id -g) -v $1:/data/ quay.io/biocontainers/qualimap:2.2.2b--1 \
 qualimap rnaseq -bam $1/$2 -gtf $1/$3 \
 -outformat PDF:HTML -outfile Qualimap_$2 \
--outdir $1/results_qualimap #> $1/results_$2/run.log
+-outdir $1/results_qualimap > $1/results_qualimap/run.log 2>&1 &
