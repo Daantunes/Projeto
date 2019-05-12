af6caa20d0de1eec16bcf794cd35e4ee6f46985e
Daantunes <50405775+Daantunes@users.noreply.github.com>
2019-05-12 10:38:00 +0100

    Create create_adaptor.sh

---
+++ rna_seq/create_adaptor.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ "$1" == "-h" ]; then
+  echo "<Pathway to fastq files>"
+  exit 0
+fi
+
+cp /adapters/TruSeq2-SE.fa $1/TruSeqALL-SE.fa
+chmod 777 $1/TruSeqALL-SE.fa
+printf "\n" >> $1/TruSeqALL-SE.fa
+cat /adapters/TruSeq3-SE.fa >> $1/TruSeqALL-SE.fa
