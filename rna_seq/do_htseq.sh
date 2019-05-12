48566d64a66ee6e802f69f3b3dd46bfd06f4bca3
Daantunes <50405775+Daantunes@users.noreply.github.com>
2019-05-12 18:22:24 +0100

    Update do_htseq.sh

--- rna_seq/do_htseq.sh
+++ rna_seq/do_htseq.sh
@@ -10,6 +10,6 @@ docker pull quay.io/biocontainers/htseq:0.11.2--py27h637b7d7_1
 
 chmod 777 $1
 
-docker run --rm --user $(id -u):$(id -g) -v $1:/data/ biocontainers/htseq:0.11.2--py27h637b7d7_1 htseq-count \
+docker run --rm --user $(id -u):$(id -g) -v $1:/data/ quay.io/biocontainers/htseq:0.11.2--py27h637b7d7_1 htseq-count \
 -m=intersection-strict --stranded=no -t=gene -o=/data/htseq_counts.sam /data/Aligned.out.sam /data/Mus_musculus_c57bl6nj.C57BL_6NJ_v1.96.gtf \
 > $1/runhtseq.log 2>&1 &
