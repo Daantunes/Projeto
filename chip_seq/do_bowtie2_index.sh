529c31d68c4f9f62e52388b299c2dfa23ceed680
Daantunes <50405775+Daantunes@users.noreply.github.com>
2019-05-12 20:36:10 +0100

    Update do_bowtie2_index.sh

--- chip_seq/do_bowtie2_index.sh
+++ chip_seq/do_bowtie2_index.sh
@@ -9,6 +9,8 @@ fi
 docker pull quay.io/biocontainers/bowtie2:2.3.5--py27he860b03_0
 
 chmod 777 $1
+mkdir $1/genome
+chmod 777 $1/genome
 
 docker run --rm --user $(id -u):$(id -g) -v $1:/data/ quay.io/biocontainers/bowtie2:2.3.4.3--py36h2d50403_0 bowtie2-build \
-/data/$2 /data/$2 > $1/runbowtie2index.log 2>&1 &
+/data/$2 /data/genome/$2 > $1/runbowtie2index.log 2>&1 &
