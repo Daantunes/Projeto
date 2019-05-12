aec20e9ab9c29bbf03f6c9f380dd948049dd86b3
Daantunes <50405775+Daantunes@users.noreply.github.com>
2019-05-12 20:14:23 +0100

    Update do_bowtie2_index.sh

--- chip_seq/do_bowtie2_index.sh
+++ chip_seq/do_bowtie2_index.sh
@@ -1,4 +1,14 @@
+#!/bin/bash
+#12-05-2019
+
+if [ "$1" == "-h" ]; then
+  echo "<Pathway to fastq files> <Name of the SAM file> <Name os the GTF/GFF file>"
+  exit 0
+fi
+
 docker pull quay.io/biocontainers/bowtie2:2.3.5--py27he860b03_0
 
-docker run -d -t -v $1:/data/ quay.io/biocontainers/bowtie2:2.3.4.3--py36h2d50403_0 bowtie2-build 
-/data/$2 
+chmod 777 $1
+
+docker run --rm --user $(id -u):$(id -g) -v $1:/data/ quay.io/biocontainers/bowtie2:2.3.4.3--py36h2d50403_0 bowtie2-build \
+/data/$2 > $1/runbowtie2index.log 2>&1 &
