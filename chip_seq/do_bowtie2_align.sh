1071429e5c4d1eed0d4461959c1cf7e500b68504
Daantunes <50405775+Daantunes@users.noreply.github.com>
2019-05-31 15:35:06 +0100

    Update do_bowtie2_align.sh

--- chip_seq/do_bowtie2_align.sh
+++ chip_seq/do_bowtie2_align.sh
@@ -10,8 +10,12 @@ fi
 docker pull quay.io/biocontainers/bowtie2:2.3.5--py27he860b03_0
 
 chmod 777 $1
-mkdir $1/genome
-chmod 777 $1/genome
+mkdir -p $1/results
+chmod 777 $1/results
 
 docker run --rm --user $(id -u):$(id -g) -v $1:/data/ quay.io/biocontainers/bowtie2:2.3.4.3--py36h2d50403_0 bowtie2-build \
-/data/$2 /data/genome/$2 > $1/runbowtie2index.log 2>&1 &
+-p 15 -q --no-unal -x /data/genome -U /data/$2 -S /data/results > $1/results/runbowtie2align.log 2>&1 &
+
+docker run --rm --user $(id -u):$(id -g) -v /home/dantunes/chip_seq:/data/ quay.io/biocontainers/bowtie2:2.3.4.3--py36h2d50403_0 \
+bowtie2-build -p 15 -q --no-unal -x /data/genome -U /data/T_SRR3737445.fastq.gz -S /data/results \
+> /home/dantunes/chip_seq/results/runbowtie2align.log 2>&1 &
