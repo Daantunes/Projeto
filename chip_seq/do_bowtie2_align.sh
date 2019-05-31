a631321e3b85eaed352e507196857f7f6eaa497d
Daantunes <50405775+Daantunes@users.noreply.github.com>
2019-05-31 18:33:30 +0100

    Update do_bowtie2_align.sh

--- chip_seq/do_bowtie2_align.sh
+++ chip_seq/do_bowtie2_align.sh
@@ -3,7 +3,7 @@
 #12-05-2019
 
 if [ "$1" == "-h" ]; then
-  echo "<Pathway to fastq files> <Name of the fasta file used in index> <Name of the fastq file>"
+  echo "<Pathway to index files> <Name of the fasta file used in index> <Name of the fastq file>"
   exit 0
 fi
 
@@ -13,8 +13,8 @@ chmod 777 $1
 mkdir -p $1/results
 chmod 777 $1/results
 
-docker run --rm --user $(id -u):$(id -g) -v $1:/data/ quay.io/biocontainers/bowtie2:2.3.4.3--py36h2d50403_0 bowtie2 \
+docker run -d --rm --user $(id -u):$(id -g) -v $1:/data/ quay.io/biocontainers/bowtie2:2.3.4.3--py36h2d50403_0 bash -c 'bowtie2 \
 -p 15 -q --no-unal \
--x /data/genome/$1 \
+-x /data/genome/$2 \
 -U /data/$3 \
--S /data/results/counts_bowtie2.sam > $1/results/runbowtie2align.log 2>&1 &
+-S /data/results/counts_$3.sam > /data/results/counts_$3.txt'
