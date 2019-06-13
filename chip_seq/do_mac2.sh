ed8c70cad2c4b0a4cc5ecf692c8600d3e5852bdd
Daantunes <50405775+Daantunes@users.noreply.github.com>
2019-06-13 15:51:25 +0100

    Update do_mac2.sh

--- chip_seq/do_mac2.sh
+++ chip_seq/do_mac2.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+#13-06-2019
 
+if [ "$1" == "-h" ]; then
+  echo "<Pathway to index files> <Name of the sam/bam file>"
+  exit 0
+fi
+
+docker pull quay.io/biocontainers/macs2:2.1.2--py27r351h14c3975_1
+
+docker run -d --rm --user $(id -u):$(id -g) -v $1:/data/ quay.io/biocontainers/macs2:2.1.2--py27r351h14c3975_1 bash -c 'macs2 \
+callpeak -t $2'
+
+docker run -d --rm --user $(id -u):$(id -g) -v /home/dantunes/chip_seq/results:/data/ quay.io/biocontainers/macs2:2.1.2--py27r351h14c3975_1 bash -c 'macs2 \
+callpeak -t counts_T_SRR3737445.fastq.gz.sam'
