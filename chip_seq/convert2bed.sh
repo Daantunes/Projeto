8c4584de2bab71dd675d00cd37fd074acbc86ab0
Daantunes <50405775+Daantunes@users.noreply.github.com>
2019-06-19 16:42:57 +0100

    Update convert2bed.sh

--- chip_seq/convert2bed.sh
+++ chip_seq/convert2bed.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
+#19-06-2019
 
+if [ "$1" == "-h" ]; then
+  echo "<Pathway to index files> <Name of the fasta file used in index> <Name of the fastq file>"
+  exit 0
+fi
+
+docker pull quay.io/biocontainers/bedtools:2.23.0--hdbcaa40_3
+
+docker run -d --rm --user $(id -u):$(id -g) -v $1:/data/ quay.io/biocontainers/bedtools:2.23.0--hdbcaa40_3 bash -c 'bowtie2 \
+-p 15 -q --no-unal \
+-x /data/genome/$2 \
+-U /data/$3 \
+-S /data/results/counts_$3.sam'
+
+
+docker run -d --rm --user $(id -u):$(id -g) -v $1:/data/ quay.io/biocontainers/bedtools:2.23.0--hdbcaa40_3 bedtools bamToBed -i reads.bam > reads.bed
