f39942b9e9e1e82170c64fb1ffcfb018952526f4
Daantunes <50405775+Daantunes@users.noreply.github.com>
2019-06-20 18:48:19 +0100

    Update do_mac2.sh

--- chip_seq/do_mac2.sh
+++ chip_seq/do_mac2.sh
@@ -8,10 +8,7 @@ fi
 
 docker pull quay.io/biocontainers/macs2:2.1.2--py27r351h14c3975_1
 
-docker run -d --rm --user $(id -u):$(id -g) -v $1:/data/ quay.io/biocontainers/macs2:2.1.2--py27r351h14c3975_1 bash -c 'macs2 \
-callpeak -t $2'
-
-docker run -d --rm --user $(id -u):$(id -g) -v /home/dantunes/chip_seq/results:/data/ quay.io/biocontainers/macs2:2.1.2--py27r351h14c3975_1 macs2 callpeak -t /data/counts_T_SRR3737446.fastq.gz.sam -c /data/counts_T_SRR3737447.fastq.gz.sam
+docker run -d --rm --user $(id -u):$(id -g) -v $1:/data/ quay.io/biocontainers/macs2:2.1.2--py27r351h14c3975_1 macs2 \
+callpeak -t /data/$2 --outdir /data/
 
 
-#
