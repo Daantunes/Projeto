efdbcfebaf727b1693a91d00a02a9746994a98ce
Daantunes <50405775+Daantunes@users.noreply.github.com>
2019-06-20 23:52:27 +0100

    Update do_qualimap.sh

--- rna_seq/do_qualimap.sh
+++ rna_seq/do_qualimap.sh
@@ -2,19 +2,16 @@
 #11-05-2019
 
 if [ "$1" == "-h" ]; then
-  echo "<Pathway to BAM file> <Name of the SAM file> <Name of the GTF file>"
+  echo "<Pathway to SAM file> <Name of the SAM file> <Name of the GTF file>"
   exit 0
 fi
 
 docker pull quay.io/biocontainers/qualimap:2.2.2b--1
 
-docker_id=$(docker run -d --rm --user $(id -u):$(id -g) -e INPUT1=$2 -e INPUT2=$3 -v $1:/data/ quay.io/biocontainers/qualimap:2.2.2b--1 \
-bash -c "qualimap \
-rnaseq -bam /data/$INPUT1 \
--gtf /data/$INPUT2 \
+docker run --rm --user $(id -u):$(id -g) -v $1:/data/ quay.io/biocontainers/qualimap:2.2.2b--1 qualimap \
+rnaseq -bam /data/$2 \
+-gtf /data/$3 \
 -outformat PDF:HTML \
--outfile /data/Qualimap_$INPUT1 \
+-outfile /data/Qualimap_$2 \
 -outdir /data/results_qualimap \
---java-mem-size=5G > /data/results_qualimap/run.log &")
-
-echo $docker_id
+--java-mem-size=5G > $1/results_qualimap/run.log 2>&1 &
