1d5d35e7d68fbfc00057965e93f59e58f930e353
Daantunes <50405775+Daantunes@users.noreply.github.com>
2019-06-20 23:25:00 +0100

    Update do_STAR_index.sh

--- rna_seq/do_STAR_index.sh
+++ rna_seq/do_STAR_index.sh
@@ -12,17 +12,15 @@ mkdir $1/genome
 
 chmod 777 $1/genome
 
-docker_id=$(docker run -d --rm --user $(id -u):$(id -g) -e INPUT1=$2 -e INPUT2=$3 -v $1:/data/ quay.io/biocontainers/star:2.7.0f--0 \
-bash -c "STAR \
+docker run --rm --user $(id -u):$(id -g) -v $1:/data/ quay.io/biocontainers/star:2.7.0f--0 STAR \
 --runThreadN 16 \
 --runMode genomeGenerate \
 --genomeDir /data/genome \
---genomeFastaFiles /data/$INPUT1 \
---sjdbGTFfile /data/$INPUT2 \
+--genomeFastaFiles /data/$2 \
+--sjdbGTFfile /data/$3 \
 --outFileNamePrefix /data/genome/ \
---sjdbOverhang 50 > /data/genome/runSTARindex.log &")
+--sjdbOverhang 50 > $1/genome/runSTARindex.log 2<&1 &
 
-echo $docker_id
 
 # Retirado de: ftp://ftp.ensembl.org/pub/release-96/fasta/mus_musculus_c57bl6nj/dna/
 # wget ftp://ftp.ensembl.org/pub/release-96/fasta/mus_musculus_c57bl6nj/dna/Mus_musculus_c57bl6nj.C57BL_6NJ_v1.dna.toplevel.fa.gz
