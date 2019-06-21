9f2dacb38f77e11d956ea48297b0ef6eb29b8f65
Daantunes <50405775+Daantunes@users.noreply.github.com>
2019-06-21 10:12:36 +0100

    Update do_STAR_map.sh

--- rna_seq/do_STAR_map.sh
+++ rna_seq/do_STAR_map.sh
@@ -10,15 +10,12 @@ mkdir $1/results_$2
 chmod 777 $1/genome
 chmod 777 $1/results_$2
 
-docker_id=$(docker run -d --rm --user $(id -u):$(id -g) -e INPUT=$2 -v $1:/data/ quay.io/biocontainers/star:2.7.0f--0 \
-bach -c "STAR \
+docker run --rm --user $(id -u):$(id -g) -v $1:/data/ quay.io/biocontainers/star:2.7.0f--0 STAR \
 --runThreadN 16 \
 --genomeDir /data/genome \
---readFilesIn /data/$INPUT \
+--readFilesIn /data/$2 \
 --readFilesCommand gunzip -c \
---outFileNamePrefix /data/results_$INPUT/ > /data/results_$INPUT/run.log &")
-
-echo $docker_id
+--outFileNamePrefix /data/results_$2/ > $1/results_$2/run.log 2>&1 &
 
 
 
