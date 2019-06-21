952d534ddaa633c235ad18a41b7b117a4536d262
Daantunes <50405775+Daantunes@users.noreply.github.com>
2019-06-21 17:51:29 +0100

    Create dif_analysis.R

---
+++ rna_seq/dif_analysis.R
@@ -0,0 +1,65 @@
+library(DESeq2)
+library(edgeR)
+
+counts = read.table('COUNTS.tab', h=T, row.names=1)
+condition = factor(c('WT','WT','WT','KO','KO','KO'))
+
+#Remove Lines With no Counts
+dim(counts)
+counts = counts[rowSums(counts)>1,]
+dim(counts)
+
+sampleTable=data.frame(c('WT','WT','WT','KO','KO','KO'))
+colnames(sampleTable)[1]= 'condition'
+rownames(sampleTable)=colnames(counts)
+
+#DESeq2
+dds = DESeqDataSetFromMatrix(countData = counts ,colData = sampleTable, 
+                              design = ~ condition)
+dds = DESeq(dds)
+
+resFDR = results(dds, contrast=c("condition","KO","WT"), alpha = 0.05)
+summary(resFDR)
+# know how many genes had adjusted p-value < 0.05
+table(resFDR$padj < 0.05) #TRUE says how many
+resFDR = as.data.frame(resFDR)
+resFDR = resFDR[!is.na(resFDR$padj),]
+# select just genes < 0.05:
+resFDR = resFDR[resFDR$padj < 0.05,]
+dim(resFDR)
+
+resFDR.down = resFDR[resFDR$log2FoldChange < 1,]
+dim(resFDR.down)
+resFDR.down
+
+resFDR.up = resFDR[resFDR$log2FoldChange > 1,]
+dim(resFDR.up)
+resFDR.up
+
+
+#EdgeR
+y <- DGEList(counts=counts[,1:6], group = condition)
+y <- calcNormFactors(y)
+design <- model.matrix(~condition)
+
+y <- estimateDisp(y,design)
+et <- exactTest(y, pair=c("WT","KO"))
+out <- topTags(et, n=Inf)
+
+down <- out$table$FDR <= 0.05 & out$table$logFC <= -1
+dim(out[down,])
+out[down,]
+
+up <- out$table$FDR <= 0.05 & out$table$logFC >= 1
+dim(out[up,])
+out[up,]
+
+
+#Compare which genes are the same with both methods
+deseq.up=row.names(resFDR.up)
+edger.up=row.names(out[up,])
+deseq.up[deseq.up %in% edger.up]
+
+deseq.down=row.names(resFDR.down)
+edger.down=row.names(out[down,])
+deseq.down[deseq.down %in% edger.down]
