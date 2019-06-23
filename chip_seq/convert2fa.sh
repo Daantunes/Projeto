42c15cdfa2bbbb83f7a2d501707f548dd28b30d9
Daantunes <50405775+Daantunes@users.noreply.github.com>
2019-06-23 17:24:45 +0100

    Update convert2fa.sh

--- chip_seq/convert2fa.sh
+++ chip_seq/convert2fa.sh
@@ -2,14 +2,16 @@
 #19-06-2019
 
 if [ "$1" == "-h" ]; then
-  echo "<Pathway to index files> <Name of the fasta file used in index> <Name of the bed file>"
+  echo "<Pathway to index files> <Name of the fasta file used in index> <Name of the narrowPeak file>"
   exit 0
 fi
 
 docker pull quay.io/biocontainers/bedtools:2.23.0--hdbcaa40_3
 
+cut -f1,2,3 $1/$3 > $1/$3.bed
+
 docker run --rm --user $(id -u):$(id -g) -v $1:/data/ quay.io/biocontainers/bedtools:2.23.0--hdbcaa40_3 bedtools getfasta \
 -fi /data/$2 \
--bed /data/$3 \
+-bed /data/$3.bed \
 -fo /data/$3.fa
 
