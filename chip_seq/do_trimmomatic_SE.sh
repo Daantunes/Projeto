f0dac6e66eca1aed413212356f2ae7a2bf0a5902
Daantunes <50405775+Daantunes@users.noreply.github.com>
2019-06-23 17:12:50 +0100

    Update do_trimmomatic_SE.sh

--- chip_seq/do_trimmomatic_SE.sh
+++ chip_seq/do_trimmomatic_SE.sh
@@ -9,12 +9,11 @@ docker pull quay.io/biocontainers/trimmomatic:0.39--1
 
 chmod 777 $1
 
-docker run --rm --user $(id -u):$(id -g) -v /usr/share/trimmomatic:/adapters/ \
--v $1:/data/ quay.io/biocontainers/trimmomatic:0.39--1 \
+docker run --rm --user $(id -u):$(id -g) -v $1:/data/ quay.io/biocontainers/trimmomatic:0.39--1 \
 trimmomatic SE /data/$2 /data/T_$2 \
-ILLUMINACLIP:$1/TruSeqALL-SE.fa:2:0:10 \
+ILLUMINACLIP:/data/TruSeqALL-SE.fa:2:0:10 \
 LEADING:0 \
 TRAILING:0 \
 SLIDINGWINDOW:0:0 \
-MINLEN:36 > run$2.log 2>&1 &
+MINLEN:36 > $1/run$2.log 2>&1 &
 
