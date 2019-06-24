fdbdcaa8afe655b86a6868c6dfc93bd318b3b275
Daantunes <50405775+Daantunes@users.noreply.github.com>
2019-06-24 11:18:13 +0100

    Update do_macs2.sh

--- chip_seq/do_macs2.sh
+++ chip_seq/do_macs2.sh
@@ -2,14 +2,14 @@
 #13-06-2019
 
 if [ "$1" == "-h" ]; then
-  echo "<Pathway to index files> <Name of the sam/bam file>"
+  echo "<Pathway to index files> <Name of the sam/bam file> <Name of the control sam/bam file"
   exit 0
 fi
 
 docker pull quay.io/biocontainers/macs2:2.1.2--py27r351h14c3975_1
 
 docker run --rm --user $(id -u):$(id -g) -v $1:/data/ quay.io/biocontainers/macs2:2.1.2--py27r351h14c3975_1 macs2 \
-callpeak -t /data/$2 --outdir /data/ > $1/counts_$2.log 2<&1 &
+callpeak -t /data/$2 -c /data/$3 -n $2 --outdir /data/ > $1/counts_$2.log 2<&1 &
 
 
 
