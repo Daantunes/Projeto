0e3b68e49ac96c30011a50c86e37a96b83748e7c
Daantunes <50405775+Daantunes@users.noreply.github.com>
2019-06-25 20:01:45 +0100

    Update compilation_counts.sh

--- rna_seq/compilation_counts.sh
+++ rna_seq/compilation_counts.sh
@@ -8,12 +8,12 @@ fi
 
 mkdir $1/final_results
 chmod 777 $1/final_results
-grep "^MGP" $1/results_T_SRR3737439.fastq.gz/htseq_counts.readcounts > /$1/final_results/C439.readcounts
-grep "^MGP" $1/results_T_SRR3737440.fastq.gz/htseq_counts.readcounts > /$1/final_results/C440.readcounts
-grep "^MGP" $1/results_T_SRR3737441.fastq.gz/htseq_counts.readcounts > /$1/final_results/C441.readcounts
-grep "^MGP" $1/results_T_SRR3737442.fastq.gz/htseq_counts.readcounts > /$1/final_results/C442.readcounts
-grep "^MGP" $1/results_T_SRR3737443.fastq.gz/htseq_counts.readcounts > /$1/final_results/C443.readcounts
-grep "^MGP" $1/results_T_SRR3737444.fastq.gz/htseq_counts.readcounts > /$1/final_results/C444.readcounts
+grep "^ENS" $1/results_T_SRR3737439.fastq.gz/htseq_counts.readcounts > /$1/final_results/C439.readcounts
+grep "^ENS" $1/results_T_SRR3737440.fastq.gz/htseq_counts.readcounts > /$1/final_results/C440.readcounts
+grep "^ENS" $1/results_T_SRR3737441.fastq.gz/htseq_counts.readcounts > /$1/final_results/C441.readcounts
+grep "^ENS" $1/results_T_SRR3737442.fastq.gz/htseq_counts.readcounts > /$1/final_results/C442.readcounts
+grep "^ENS" $1/results_T_SRR3737443.fastq.gz/htseq_counts.readcounts > /$1/final_results/C443.readcounts
+grep "^ENS" $1/results_T_SRR3737444.fastq.gz/htseq_counts.readcounts > /$1/final_results/C444.readcounts
 
 echo "geneid WT1 WT2 WT3 KO1 KO2 KO3" > $1/final_results/COUNTS.tab
 
