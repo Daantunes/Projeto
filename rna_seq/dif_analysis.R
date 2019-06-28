224b048b55b9437559724f956d4aa04e257838b6
Daantunes <50405775+Daantunes@users.noreply.github.com>
2019-06-28 01:52:19 +0100

    Update dif_analysis.R

--- rna_seq/dif_analysis.R
+++ rna_seq/dif_analysis.R
@@ -54,6 +54,9 @@ up <- out$table$FDR <= 0.05 & out$table$logFC >= 1
 dim(out[up,])
 out[up,]
 
+rna=c(row.names(out[down,]),row.names(out[up,]))
+write.table(rna, file="rna.txt", quote=FALSE, row.names=FALSE, col.names=FALSE)
+
 symbol.down.all = mapIds(org.Mm.eg.db, keys=row.names(out[down,]), column="SYMBOL", keytype="ENSEMBL", multiVals="first")
 symbol.down=symbol.down.all[which(!is.na(symbol.down.all))]
 length(symbol.down)
